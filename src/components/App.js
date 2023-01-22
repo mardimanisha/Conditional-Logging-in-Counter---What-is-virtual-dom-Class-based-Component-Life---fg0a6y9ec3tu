c1880bb82060b040545a443d99eb5df24c934ca8
Manisha Mardi <118474184+mardimanisha@users.noreply.github.com>
2023-01-22 21:44:48 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,10 @@ class App extends React.Component{
     this.state = {count:0}
     this.handleClick = this.handleClick.bind(this)
   }
+  
+  shouldComponentUpdate(nextProps,nextState){
+  return nextState.count%2==0
+  }
 
 
   render(){
